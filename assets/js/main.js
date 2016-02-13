f686a614e77179b1dd797d0d5ea75d5bdc16339b
Ivo Zinenda <izinenda@outlook.com>
2016-02-13 09:54:55 +0200

    new imgs slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,10 @@
     $('.button-collapse').sideNav();
     $('.modal-trigger').leanModal();
     $('.materialboxed').materialbox();
-    $('.slider').slider();
+    $('.slider').slider({
+        indicators: true,
+        height: 500
+    });
     $('.slider').slider('next');
     $('.slider').slider('prev');
     $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
