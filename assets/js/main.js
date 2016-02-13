bb91faab15caa3b9fa76c8390247c60faa361472
Ivo Zinenda <izinenda@outlook.com>
2016-02-13 09:24:45 +0200

    few bugs fixed and slider improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@
     $('.modal-trigger').leanModal();
     $('.materialboxed').materialbox();
     $('.slider').slider();
+    $('.slider').slider('next');
+    $('.slider').slider('prev');
     $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
     $('.scrollspy').scrollSpy();
 
